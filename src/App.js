028f2dfe3fa86d7ff314b582b49deca3076b200d
Rodney Dumee <rdu@netcompany.com>
2020-07-29 11:16:56 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. <br/>
-          Done by RDumee
+          Done by RDumee feature
         </p>
         <a
           className="App-link"
